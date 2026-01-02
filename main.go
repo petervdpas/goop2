2078f0448cac874186e90b87945a2713e9216f4c
Peter van de Pas <peter.vdpas@protonmail.com>
2026-01-02 13:24:17 +0100

    trying to set icon

--- main.go
+++ main.go
@@ -3,14 +3,19 @@ package main
 
 import (
 	"embed"
+	"log"
 
 	"github.com/wailsapp/wails/v2"
 	"github.com/wailsapp/wails/v2/pkg/options"
 	"github.com/wailsapp/wails/v2/pkg/options/assetserver"
+	"github.com/wailsapp/wails/v2/pkg/options/linux"
 )
 
 //go:embed all:frontend/dist
 var assets embed.FS
+
+//go:embed build/appicon.png
+var appIcon []byte
 
 func main() {
 	app := NewApp()
@@ -19,16 +24,20 @@ func main() {
 		Title:  "Goop",
 		Width:  1200,
 		Height: 800,
+
 		AssetServer: &assetserver.Options{
 			Assets: assets,
 		},
-		OnStartup: app.startup,
-		Bind: []any{
-			app,
-		},
-	})
 
+		// Linux runtime window icon (closest to Electron behaviour)
+		Linux: &linux.Options{
+			Icon: appIcon,
+		},
+
+		OnStartup: app.startup,
+		Bind:      []any{app},
+	})
 	if err != nil {
-		println(err.Error())
+		log.Fatal(err)
 	}
 }
