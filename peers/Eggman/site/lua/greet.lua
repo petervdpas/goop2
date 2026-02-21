9be44fe35c2ab833242d8bed7ef8a700a9881294
Peter van de Pas <peter.vdpas@protonmail.com>
2026-02-21 17:23:13 +0100

    peer creation fix

--- peers/Eggman/site/lua/greet.lua
+++
@@ -1,9 +0,0 @@
--- greet.lua — Responds differently based on who's calling.
-function handle(args)
-    local label = goop.peer.label
-    if label ~= "" then
-        return "Hello, " .. label .. "!"
-    else
-        return "Hello, peer " .. string.sub(goop.peer.id, 1, 8) .. "...!"
-    end
-end
