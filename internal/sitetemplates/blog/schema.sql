6ee739754e26e02eaeef56fde1aef29955cb57d7
Peter van de Pas <peter.vdpas@protonmail.com>
2026-02-22 10:57:45 +0100

    fancy blog

--- internal/sitetemplates/blog/schema.sql
+++ internal/sitetemplates/blog/schema.sql
@@ -10,3 +10,13 @@ CREATE TABLE posts (
   slug         TEXT,
   published    INTEGER DEFAULT 1
 );
+
+CREATE TABLE blog_config (
+  _id          INTEGER PRIMARY KEY AUTOINCREMENT,
+  _owner       TEXT NOT NULL,
+  _owner_email TEXT DEFAULT '',
+  _created_at  DATETIME DEFAULT CURRENT_TIMESTAMP,
+  _updated_at  DATETIME DEFAULT CURRENT_TIMESTAMP,
+  key          TEXT NOT NULL,
+  value        TEXT NOT NULL DEFAULT ''
+);
