1b5db435962708cdb2dffd082c2f8c81059ddac9
Peter van de Pas <peter.vdpas@protonmail.com>
2026-03-22 02:45:23 +0100

    node test

---
+++ internal/p2p/node_test.go
@@ -0,0 +1,51 @@
+package p2p
+
+import (
+	"io"
+	"testing"
+
+	libp2p "github.com/libp2p/go-libp2p"
+	ymux "github.com/libp2p/go-libp2p/p2p/muxer/yamux"
+	yamux "github.com/libp2p/go-yamux/v4"
+)
+
+func TestNodeLibp2pOptions_NoConflict(t *testing.T) {
+	ymuxCfg := yamux.DefaultConfig()
+	ymuxCfg.KeepAliveInterval = YamuxKeepAlive
+	ymuxCfg.LogOutput = io.Discard
+
+	h, err := libp2p.New(
+		libp2p.ListenAddrStrings("/ip4/127.0.0.1/tcp/0"),
+		libp2p.Muxer(ymux.ID, (*ymux.Transport)(ymuxCfg)),
+		libp2p.DefaultTransports,
+	)
+	if err != nil {
+		t.Fatalf("libp2p.New with production options failed: %v", err)
+	}
+	defer h.Close()
+
+	if h.ID() == "" {
+		t.Fatal("expected valid peer ID")
+	}
+}
+
+func TestNodeLibp2pOptions_CanDialWSS(t *testing.T) {
+	ymuxCfg := yamux.DefaultConfig()
+	ymuxCfg.KeepAliveInterval = YamuxKeepAlive
+	ymuxCfg.LogOutput = io.Discard
+
+	h, err := libp2p.New(
+		libp2p.ListenAddrStrings("/ip4/127.0.0.1/tcp/0"),
+		libp2p.Muxer(ymux.ID, (*ymux.Transport)(ymuxCfg)),
+		libp2p.DefaultTransports,
+	)
+	if err != nil {
+		t.Fatal(err)
+	}
+	defer h.Close()
+
+	protos := h.Mux().Protocols()
+	if len(protos) == 0 {
+		t.Fatal("expected registered protocols")
+	}
+}
